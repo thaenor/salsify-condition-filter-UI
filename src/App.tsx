b20c5b002dd9b514b1d52c6af606ca3611888a8d
thaenor <3703930+thaenor@users.noreply.github.com>
2026-05-12 10:47:39 +0200

    add tailwind and shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import { NavBar } from './components/NavBar'
+import { Footer } from './components/Footer'
+import { PageTitle } from './components/PageTitle'
+import { FilterBar } from './components/FilterBar'
+import { ProductTable } from './components/ProductTable'
+
 export default function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="min-h-screen flex flex-col bg-background">
+      <NavBar />
+      <main className="flex-1 max-w-5xl mx-auto w-full px-6 py-10 flex flex-col gap-8">
+        <PageTitle />
+        <FilterBar />
+        <ProductTable />
+      </main>
+      <Footer />
+    </div>
+  )
 }
